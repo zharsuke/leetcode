aa70ab6c10b23d3cf6ae6dee7db559c6c57fc5b6
zharsuke <alazharisqi@gmail.com>
2023-10-30 19:13:31 +0700

    feat: twoSum done with bruteforce method

--- algorithms/two-sum/twoSum.java
+++ algorithms/two-sum/twoSum.java
@@ -1,9 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
         return null;
     }
 
     public static void main(String[] args) {
-        
+        int[] num = {2,7,11,15};
+        int target = 9;
+        int[] result = new Solution().twoSum(num, target);
+        System.out.println(result[0] + " " + result[1]);
     }
 }
