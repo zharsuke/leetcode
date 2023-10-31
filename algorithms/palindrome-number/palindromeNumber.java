8e6821c475d4d7c6eb1a413475c8f3d596e73d53
zharsuke <alazharisqi@gmail.com>
2023-10-31 19:12:01 +0700

    feat: palindrome number done with while loop method

--- algorithms/palindrome-number/palindromeNumber.java
+++ algorithms/palindrome-number/palindromeNumber.java
@@ -1,5 +1,21 @@
-class palindromeNumber {
-    public boolean isPalindrome(int x) {
-        return true;
+public class palindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+        if (x == 0) {
+            return true;
+        }
+        int reversedNumber = 0;
+        while (x > reversedNumber) {
+            reversedNumber = reversedNumber * 10 + x % 10;
+            x = x / 10;
+        }
+        return x == reversedNumber || x == reversedNumber / 10;
+    }
+
+    public static void main(String[] args) {
+        int x = 3663;
+        boolean result = isPalindrome(x);
+        System.out.println(x);
+        System.out.println(result);
     }
 }
