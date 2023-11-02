6e09288e5e3a8f8131c3f2b344646f764c65c998
zharsuke <alazharisqi@gmail.com>
2023-11-02 14:32:29 +0700

    feat: add roman to int done

--- algorithms/roman-to-int/RomanToInt.java
+++ algorithms/roman-to-int/RomanToInt.java
@@ -1,5 +1,30 @@
+import java.util.HashMap;
+
 public class RomanToInt {
-    public int romanToInt(String s) {
-        return 0;
+    public static int romanToInt(String s) {
+        int result = 0;
+        HashMap<Character, Integer> data = new HashMap<Character, Integer>();
+        data.put('I', 1);
+        data.put('V', 5);
+        data.put('X', 10);
+        data.put('L', 50);
+        data.put('C', 100);
+        data.put('D', 500);
+        data.put('M', 1000);
+        for (int i = 0; i < s.length(); i++) {
+            if (i+1 < s.length() && data.get(s.charAt(i)) < data.get(s.charAt(i + 1))) {
+                result -= data.get(s.charAt(i));
+            } else {
+                result += data.get(s.charAt(i));
+
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String data = "IX";
+        int result = romanToInt(data);
+        System.out.println(data + " = " + result);
     }
 }
