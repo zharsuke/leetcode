31fc663dba7b39008fe20cfd23d3f2ee6456f973
zharsuke <alazharisqi@gmail.com>
2023-11-03 14:26:19 +0700

    feat: add roman to int python done

--- algorithms/roman-to-int/romanToInt.py
+++ algorithms/roman-to-int/romanToInt.py
@@ -1,3 +1,22 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        return None
+        result = 0
+        data = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        for i in range(len(s)):
+            if i+1 < len(s) and data.get(s[i]) < data.get(s[i+1]):
+                result -= data.get(s[i])
+            else:
+                result += data.get(s[i])
+        return result
+    
+data = "IX"
+result = Solution().romanToInt(data)
+print(data, " = ", result)
