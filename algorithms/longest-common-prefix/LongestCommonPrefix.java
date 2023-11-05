18503d1038829adee2fed0dcf5f0b1203126dc63
zharsuke <alazharisqi@gmail.com>
2023-11-05 15:28:23 +0700

    feat: longest common prefix done with java

--- algorithms/longest-common-prefix/LongestCommonPrefix.java
+++ algorithms/longest-common-prefix/LongestCommonPrefix.java
@@ -1,7 +1,22 @@
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-     String result = "";
+        if (strs == null || strs.length == 0) return "";
+        String result = "";
+        for (int i = 0; i < strs[0].length(); i++) {
+            char currentChar = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != currentChar) {
+                    return result;
+                }
+            }
+            result += currentChar;
+        }
+        return result;
+    }
 
-     return result;
-    }    
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        String result = new LongestCommonPrefix().longestCommonPrefix(strs);
+        System.out.println(result);
+    }
 }
