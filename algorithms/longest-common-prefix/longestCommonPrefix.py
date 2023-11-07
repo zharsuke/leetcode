b04555f9f261271294c9a439b070d79d32e47700
zharsuke <alazharisqi@gmail.com>
2023-11-07 13:35:20 +0700

    feat: solve longest common prefix problem with python (done)

--- algorithms/longest-common-prefix/longestCommonPrefix.py
+++ algorithms/longest-common-prefix/longestCommonPrefix.py
@@ -1,3 +1,16 @@
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        return None
+        if strs == None or len(strs) == 0:
+            return ""
+        result  = ""
+        for i in range(len(strs[0])):
+            currentChar = strs[0][i]
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != currentChar:
+                    return result
+            result += currentChar
+        return result
+    
+str = ["flower","flow","flight"]
+result = Solution().longestCommonPrefix(str)
+print(str, " -> ", result)
