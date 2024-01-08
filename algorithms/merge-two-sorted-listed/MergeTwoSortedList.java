0132f73a9ba57fce9f35b1344fac767deed26bb0
zharsuke <alazharisqi@gmail.com>
2024-01-09 00:56:27 +0700

    feat: add whatever

---
+++ algorithms/merge-two-sorted-listed/MergeTwoSortedList.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class MergeTwoSortedList {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+    }
+}
